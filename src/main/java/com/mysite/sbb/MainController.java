2ae64f0cd82001af558ef13a8b6089eaf36037d9
yonguk <kimyu9827@naver.com>
2023-11-21 16:25:42 +0900

    2-8

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
